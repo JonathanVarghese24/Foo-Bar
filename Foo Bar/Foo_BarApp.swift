f3412418dd4ce3df9d0914917b9d7743c057f02b
JonathanVarghese <jvarghese@bsd220.org>
2024-08-22 10:08:34 -0500

    Initial Commit

---
+++ Foo Bar/Foo_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_BarApp.swift
+//  Foo Bar
+//
+//  Created by Jonathan V on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Foo_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
